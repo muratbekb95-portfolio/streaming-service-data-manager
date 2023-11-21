675b866f11c9cfeaec5a1a6a1ba5483768d6e7cb
muratbekb95 <muratbekb95@gmail.com>
2023-11-22 01:21:32 +0600

    Init

---
+++ src/main/java/muratbekb95portfolio/streamingservicedatamanager/DataManagerForStreamingServiceApplication.java
@@ -0,0 +1,13 @@
+package muratbekb95portfolio.streamingservicedatamanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataManagerForStreamingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataManagerForStreamingServiceApplication.class, args);
+	}
+
+}
